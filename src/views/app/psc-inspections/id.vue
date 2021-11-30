d748f35410f020f455af61486f24b74d110f7e3f
Khan-Sohail <Ksohail.sk32@gmail.com>
2021-11-30 13:25:56 +0530

    CR: Add a View PSC Inspection tab

--- src/views/app/psc-inspections/id.vue
+++ src/views/app/psc-inspections/id.vue
@@ -6,15 +6,15 @@
       <div class="content">
         <b-row>
           <b-col md="3">
-            <p class="text-muted mt-2 mb-0">Vessel Name</p>
-            <p class="text-primary text-24 line-height-1 mb-2">
-              {{ vessel.name }}
-            </p>
-          </b-col>
-          <b-col md="3">
             <p class="text-muted mt-2 mb-0">Serial No</p>
             <p class="text-primary text-24 line-height-1 mb-2">
               {{ vessel.serial_no }}
+            </p>
+          </b-col>
+          <b-col md="3">
+            <p class="text-muted mt-2 mb-0">Vessel Name</p>
+            <p class="text-primary text-24 line-height-1 mb-2">
+              {{ vessel.name }}
             </p>
           </b-col>
           <b-col md="3">
@@ -191,8 +191,7 @@
                 </b-form-group>
               </b-col>
             </b-row>
-            <div
-            >
+            <div>
               <!-- v-if="
                 form.is_deficiency_closed == 1 &&
                   form.no_of_issued_deficiencies ==
